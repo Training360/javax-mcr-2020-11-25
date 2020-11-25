64656f68f94b00e475c3b63dbf79f1970bbd70a0
iviczian <viczian.istvan@gmail.com>
2020-11-25 11:35:38 +0100

    Service bevezetése

--- src/main/java/training/employees/HelloController.java
+++ src/main/java/training/employees/HelloController.java
@@ -4,14 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDateTime;
-
 @Controller
 public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello() {
-        return "Hello Spring Boot World " + LocalDateTime.now();
+        return helloService.sayHello();
     }
 }
